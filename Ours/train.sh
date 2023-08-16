8bebd540a7ae89dbfeddd98373720f179d36c361
Your Name <you@example.com>
2023-08-16 20:16:04 +0900

    bug fix and change scaleup of decoder

--- Ours/train.sh
+++ Ours/train.sh
@@ -7,6 +7,6 @@
 #SBATCH -o suyoung01.out
 #SBATCH -e suyoung01.err
 #SBATCH --time 12:00:00
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 
 python main.py --model-name=ZoomNet --config=configs/zoomnet/zoomnet.py --datasets-info ./configs/_base_/dataset/dataset_configs.json --info demo > suyoung01.out
