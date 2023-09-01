3e80a9319c4516ac0c66da1b11eb9ccebdd5e9c5
rhtmddn100 <78678884+rhtmddn100@users.noreply.github.com>
2023-09-01 17:07:44 +0900

    change order of modules

--- Ours/methods/zoomnet/zoomnet.py
+++ Ours/methods/zoomnet/zoomnet.py
@@ -29,6 +29,46 @@ class Channel_Matching(nn.Module):
         return c5, c4, c3, c2
 
 
+
+
+## Channel Attention Layer
+class CALayer(nn.Module):
+    def __init__(self, channel, reduction=16, bias=False):
+        super(CALayer, self).__init__()
+        # global average pooling: feature --> point
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        # feature channel downscale and upscale --> channel weight
+        self.conv_du = nn.Sequential(
+            nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=bias),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=bias),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+## Channel Attention Block (CAB)
+class CAB(nn.Module):
+    def __init__(self, n_feat, kernel_size, reduction, bias, act):
+        super(CAB, self).__init__()
+        modules_body = []
+        modules_body.append(nn.Conv2d(n_feat, n_feat, kernel_size, padding=(kernel_size // 2), bias=bias))
+        modules_body.append(act)
+        modules_body.append(nn.Conv2d(n_feat, n_feat, kernel_size, padding=(kernel_size // 2), bias=bias))
+
+        self.CA = CALayer(n_feat, reduction, bias=bias)
+        self.body = nn.Sequential(*modules_body)
+
+    def forward(self, x):
+        res = self.body(x)
+        res = self.CA(res)
+        res += x
+        return res
+
+
 class GA(nn.Module):
     def __init__(self, channel, subchannel):
         super(GA, self).__init__()
@@ -70,44 +110,6 @@ class GAB(nn.Module):
 
         return y
 
-
-
-## Channel Attention Layer
-class CALayer(nn.Module):
-    def __init__(self, channel, reduction=16, bias=False):
-        super(CALayer, self).__init__()
-        # global average pooling: feature --> point
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        # feature channel downscale and upscale --> channel weight
-        self.conv_du = nn.Sequential(
-            nn.Conv2d(channel, channel // reduction, 1, padding=0, bias=bias),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(channel // reduction, channel, 1, padding=0, bias=bias),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        y = self.avg_pool(x)
-        y = self.conv_du(y)
-        return x * y
-
-## Channel Attention Block (CAB)
-class CAB(nn.Module):
-    def __init__(self, n_feat, kernel_size, reduction, bias, act):
-        super(CAB, self).__init__()
-        modules_body = []
-        modules_body.append(nn.Conv2d(n_feat, n_feat, kernel_size, padding=(kernel_size // 2), bias=bias))
-        modules_body.append(act)
-        modules_body.append(nn.Conv2d(n_feat, n_feat, kernel_size, padding=(kernel_size // 2), bias=bias))
-
-        self.CA = CALayer(n_feat, reduction, bias=bias)
-        self.body = nn.Sequential(*modules_body)
-
-    def forward(self, x):
-        res = self.body(x)
-        res = self.CA(res)
-        res += x
-        return res
 
 
 
