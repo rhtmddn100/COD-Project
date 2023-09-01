7d0097c1075b970fe39bf3d0a6d37b91ab5d32b9
Suyoung Kim <ksyo96@gmail.com>
2023-09-01 16:50:25 +0900

    delete redundant files

--- Ours/dataset/__init__.py
+++ Ours/dataset/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
 from .msi_cod import MSICOD_TestDataset, MSICOD_TrainDataset
-from .msi_sod import MSISOD_TrainDataset, MSISOD_TestDataset
--- Ours/methods/classic_methods/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2021/7/25
-# @Author  : Lart Pang
-# @GitHub  : https://github.com/lartpang
