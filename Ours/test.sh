0d991f696de36185a1982c6bfcfe373d4a21f7d2
s.j.bang <114676412+sjbinch@users.noreply.github.com>
2023-11-24 14:47:30 +0900

    Update test.sh

--- Ours/test.sh
+++ Ours/test.sh
@@ -9,7 +9,7 @@ echo 'Excute the script on GPU: ' "$1"
 
 echo 'For COD'
 python test.py --config ./configs/ento/ento.py \
-    --model-name ETNO \
+    --model-name ENTO \
     --batch-size 22 \
     --load-from /home/suyoung/PycharmProjects/COD-Project/Ours/output/ENTO_BS16_LR0.01_E100_H768_W768_OPMsgd_OPGMfinetune_SCf3_AMP_INFOdemo/pth/state_final.pth \
     --save-path ./output/ForSharing/COD_Results_1
